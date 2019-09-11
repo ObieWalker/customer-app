6427ec3c37bee1a450cd78bf8b6bbcec6cf8e8fd
ObieWalker <obinnawalker@gmail.com>
2019-09-11 15:56:34 +0100

    add authorization to customer endpoint

--- server/constants/messages.js
+++ server/constants/messages.js
@@ -18,7 +18,8 @@ const messages = {
   INVALID_PASSWORD: 'Invalid password.',
   ACCOUNT_NOT_EXIST: 'The account does not exists.',
   EVALUATION_NOT_EXIST: 'The evaluation does not exists.',
-  DEVELOPERS_STATUS_NOT_EXIST: 'There are no statuses from your developers.'
+  DEVELOPERS_STATUS_NOT_EXIST: 'There are no statuses from your developers.',
+  UNAUTHORIZED: 'You are not authorized.'
 }
 
 module.exports = messages;
